beff4e15f372b164eb5677934a13a84d4710b0d4
vynvan_server <giant_innovations@live.de>
2024-10-29 15:35:39 +0100

    Began homework

--- src/App.js
+++ src/App.js
@@ -1,36 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-function Message(props) {
-  return <p>{props.message}</p>
-}
-
-class MessageClass extends Component {
+class Message extends Component {
   render() {
     return <p>{this.props.message}</p>
   }
 }
 
+function MessageFunky(props) {
+  return <p>{props.message}</p>
+}
+
+function Header() {
+  return <div>Hallo Header</div>;
+}
+
+function LikeButton() {
+  const [likes, setLikes] = React.useState(0);
+  const onClick = () => setLikes(likes + 1);
+  return <button onClick={onClick}>Like ({likes})</button>
+}
+
+function TodoList() {
+  const [items, setItems] = React.useState([]);
+
+  function addTodo() {
+    const {value} = document.getElementById('todo-input');
+    const array = [...items, value];
+    setItems(array);
+  }
+
+  return <>
+    <ul>
+      {items.map((item, index) => <li key={index}>{item}</li>)}
+    </ul>
+    <input type="text" id="todo-input" placeholder="Neue Aufgabe hinzufügen" />
+    <button className="btn btn-primary rounded-circle" onClick={addTodo} title="Hinzufügen">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
+        <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+      </svg>
+    </button>
+  </>
+}
+
 function App() {
   return (
     <div className="App">
-      <Message message="function" />
-      <MessageClass message="class" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Message message="classic" />
+      <MessageFunky message="funky" />
+      <TodoList />
+      <LikeButton />
     </div>
   );
 }
