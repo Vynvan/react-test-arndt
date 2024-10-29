fcdf58b4a5f00e07698407fdcd1b367c4fb23aab
vynvan_server <giant_innovations@live.de>
2024-10-29 11:33:24 +0100

    Some component testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+function Message(props) {
+  return <p>{props.message}</p>
+}
+
+class MessageClass extends Component {
+  render() {
+    return <p>{this.props.message}</p>
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Message message="function" />
+      <MessageClass message="class" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
